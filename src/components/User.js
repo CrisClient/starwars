463de0c5cd2a9c43a1c17ae6687517838faf9f5c
jessica <jessicahercon@gmail.com>
2023-01-17 16:55:54 -0600

    styles added

--- src/components/User.js
+++ src/components/User.js
@@ -1,20 +1,20 @@
 
 const User=({user})=>{
     return(
-        <div className="flex flex-col">
-            <div className="text-lg ">
+        <div className="flex flex-col mt-8">
+            <div className="text-lg font-semibold border-dotted border-2 border-indigo-600">
             {user.name}
             </div>
-            <div className="flex space-x-4">
-                <ul>
-                    <li>{user.height}</li>
-                    <li>{user.mass}</li>
-                    <li>{user.hair_color}</li>
+            <div className="flex flex-col  my-6 ">
+                <ul className="" >
+                    <li className="my-3 border-double border-4 border-indigo-600"><p className="font-medium">This character's height is: </p> {user.height}</li>
+                    <li className="my-3 border-double border-4 border-indigo-600"><p className="font-medium">This character's mass is: </p>{user.mass}</li>
+                    <li className="my-3 border-double border-4 border-indigo-600"><p className="font-medium">Hair color: </p>{user.hair_color}</li>
                 </ul>
-                <ul>
-                    <li>{user.eye_color}</li>
-                    <li>{user.birth_year}</li>
-                    <li>{user.gender}</li>
+                <ul className="">
+                    <li className="my-3 border-double border-4 border-indigo-600"><p className="font-medium">Eye color: </p>{user.eye_color}</li>
+                    <li className="my-3 border-double border-4 border-indigo-600"><p className="font-medium">Birth year: </p>{user.birth_year}</li>
+                    <li className="my-3 border-double border-4 border-indigo-600"><p className="font-medium">Gender: </p>{user.gender}</li>
                 </ul>
             </div>
         </div>
