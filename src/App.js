59b4bc4d10f6760ef4f0cc8d2e363936ca07311c
crisclient <oyeecriswg@gmail.com>
2023-01-16 20:22:49 -0600

    users

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [users, setUsers] = useState(null)
 
   useEffect(() => {
     fetch("https://swapi.dev/api/people/?search=")
     .then((result) => result.json())
     .then((data) => {
-      console.log(data)
+      console.log(data.results)
     })
-  })
+  },[])
 
 
   return (
     <div className="App">
-      Hola
+      <input type="text" placeholder="Ingresa un nombre"/>
+      <button>Buscar</button>
+      <div>
+        {users.name} {users.height} {users.mass} {users.hair_color} {users.skin_color} {users.eye_color} {users.birth_year} {users.gender}
+      </div>
     </div>
   );
 }
