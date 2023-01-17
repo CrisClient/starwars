7638c7606af287c19f4b6b66e9aff0caa2a17595
sueenciso <susana.enciso@icloud.com>
2023-01-16 21:05:16 -0600

    busqueda de caracter

--- src/App.js
+++ src/App.js
@@ -4,24 +4,25 @@ import { useEffect, useState } from 'react';
 
 function App() {
 
-  const [users, setUsers] = useState(null)
+  const [users, setUsers] = useState(null);
+  const [caracter, setCaracter]=useState("");
 
   useEffect(() => {
-    fetch("https://swapi.dev/api/people/?search=")
+    fetch("https://swapi.dev/api/people/?search="+caracter)
     .then((result) => result.json())
     .then((data) => {
       console.log(data.results)
     })
-  },[])
+  },[caracter])
 
+  const caracterSearch=(e)=>{
+    setCaracter(e.target.value);
+  }
 
   return (
     <div className="App">
-      <input type="text" placeholder="Ingresa un nombre"/>
+      <input type="text" placeholder="Ingresa un nombre" onChange={caracterSearch}/>
       <button>Buscar</button>
-      <div>
-        {users.name} {users.height} {users.mass} {users.hair_color} {users.skin_color} {users.eye_color} {users.birth_year} {users.gender}
-      </div>
     </div>
   );
 }
